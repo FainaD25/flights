5beb7bf8b9db0002ecd39183f11adc9457925290
Faina <fadoms@taltech.ee>
2025-03-17 15:33:11 +0200

    endpoint

---
+++ src/main/java/org/example/flights/service/FlightService.java
@@ -0,0 +1,35 @@
+package org.example.flights.service;
+
+import org.example.flights.database.Flight;
+import org.example.flights.database.FlightDatabase;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FlightService {
+    private static final FlightDatabase flightDatabase = new FlightDatabase();
+
+    public List<Flight> getFlights(String from, String to, String date,
+                                   Double duration, Double price) {
+        List<Flight> result = flightDatabase.getFlights();
+        if (from != null) {
+            result = result.stream().filter(flight -> flight.getFrom().equals(from)).toList();
+        }
+        if (to != null) {
+            result = result.stream().filter(flight -> flight.getTo().equals(to)).toList();
+        }
+        if (date != null) {
+            result = result.stream().filter(flight -> flight.getDate().equals(date)).toList();
+        }
+        if (duration != null) {
+            result = result.stream().filter(flight -> flight.getDuration() == duration).toList();
+        }
+        if (price != null) {
+            result = result.stream().filter(flight -> flight.getPrice() == price).toList();
+        }
+        return result;
+    }
+
+}
